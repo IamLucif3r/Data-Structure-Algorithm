1e4c999b2c278e383146201a325aa13f84ee74cc
IamLucif3r <iamlucif3r@localhost.localdomain>
2021-06-21 15:31:56 +0530

    Union of Array

---
+++ 450-DSA-Cracker/Array/Codes/union-of-array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    int doUnion(int a[], int n, int b[], int m){
+        set<int>s;
+        for (int i = 0; i < n; i++)
+            s.insert(a[i]);
+        for (int i = 0; i < m; i++)
+            s.insert(b[i]);
+        return s.size();
+    }
+};
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m,n;
+        cin>>n>>m;
+        int a[n], b[m];
+        for(int i=0;i<n;i++)
+        cin>>a[i];
+        for(int i=0;i<m;i++)
+        cin>>b[i];
+        Solution ob;
+        cout<<ob.doUnion(a,n,b,m)<<endl;
+    }
+    return 0;
+
+}
