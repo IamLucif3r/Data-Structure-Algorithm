60a07d8d95fd46b6f2eaa2216a0623da114bba95
iamlucif3r <sanmol016@gmail.com>
2021-09-18 18:37:59 +0530

    Automated Commit Message

---
+++ FacePrep/Codes/karprekar.java
@@ -0,0 +1,14 @@
+// Karprekar Number:
+/* Example:
+for num- n; n^2/pow(10,n ki length),so on , if Q+R = num; it's a Karprekar number;
+
+
+*/
+import java.util.*;
+public class karprekar {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
