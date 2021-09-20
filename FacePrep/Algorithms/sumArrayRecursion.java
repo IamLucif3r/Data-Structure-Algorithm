e8a6856631f5035e9af88416f9865412b6dc496e
iamlucif3r <sanmol016@gmail.com>
2021-09-20 18:28:27 +0530

    Automated Commit Message

--- FacePrep/Algorithms/sumArrayRecursion.java
+++ FacePrep/Algorithms/sumArrayRecursion.java
@@ -5,12 +5,12 @@ public class sumArrayRecursion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = { 1, 2, 3, 4, 5 };
-        System.out.println(findSum(arr, arr.length));
+        System.out.println(calcSum(arr, arr.length));
     }
-    static int findSum(int A[], int N)
+    static int calcSum(int A[], int N)
     {
         if (N <= 0)
             return 0;
-        return (findSum(A, N - 1) + A[N - 1]);
+        return (calcSum(A, N - 1) + A[N - 1]);
     }
 }
