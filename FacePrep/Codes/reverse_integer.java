717f131d6ae31d2a8b60e2d74d503945eaff48b9
IamLucif3r <sanmol016@gmail.com>
2021-10-08 23:12:05 +0530

    Automated Commit Message

---
+++ FacePrep/Codes/reverse_integer.java
@@ -0,0 +1,34 @@
+/* 
+Given a non-negative integer N. 
+Reverse the bits of N and print the number obtained after reversing the bits.
+
+Note: The actual binary representation of the number is being considered,
+for reversing the bits. No leading 0’s, are being considered.
+Constraint:
+1 ≤ N ≤ 109
+Example 1:
+Input: 
+11
+Output:
+13
+Explanation:
+(11)10 = (1011)2.
+After reversing the bits we get:
+(1101)2 = (13)10.
+Example 2:
+Input: 
+10
+Output:
+5
+Explanation:
+(10)10 = (1010)2.
+After reversing the bits we get:
+(0101)2 = (101)2 = (5)10.
+*/
+import java.util.*;
+public class reverse_integer {
+    public static void main(String[] args) {
+        
+    }
+    
+}
