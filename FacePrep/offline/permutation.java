1a44e7dc9cc25276b7251cb58dc1a499a6e9c312
IamLucif3r <sanmol016@gmail.com>
2021-12-06 13:50:25 +0530

    Automated Commit Message

---
+++ FacePrep/offline/permutation.java
@@ -0,0 +1,15 @@
+// RECURSION: Permutation of a String
+// Use recursion when you have problems with patterns like multiplication of numbers, factorial
+/// or fabonicci series.
+public class permutation {
+    static void permutate()
+    {
+
+    }
+    public static void main(String args[])
+    {
+        String asy = "ANMOL";
+        char letters[] = word.toCharArray();
+
+    }
+}
