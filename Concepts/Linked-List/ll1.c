04348653c3c723f47329ec6b6ca68dadcb74c287
IamLucif3r <sanmol016@gmail.com>
2022-01-27 09:35:16 +0530

    Automated Commit Message

--- Concepts/Linked-List/ll1.c
+++ Concepts/Linked-List/ll1.c
@@ -30,18 +30,35 @@ void append(struct Node** head_ref, int new_data)
 // Printing the Contents of Linked List
 void printList(struct Node *node)
 {
+    printf("\n Linked List Traversal: \n");
     while(node!=NULL)
     {
-        printf("%d \n", node->data);
+        printf("%d ", node->data);
         node = node->next;
     }   
+}
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node ->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+void deleteNode(struct Node** head_ref, int key)
+{
+    struct Node *temp = *head_ref, *prev;
+    *head_ref = temp->next;
+    
 }
 int main()
 {
     struct Node* head = NULL;
     //append(&head, 6);
-    append(&head, 12);
-    append(&head, 15);
+    //append(&head, 12);
+    //append(&head, 15);
+    push(&head, 13);
+    push(&head, 14);
+    push(&head, 15);
     printf("\n LINKED LIST CONTAINS: \n");
     printList(head); 
 }
